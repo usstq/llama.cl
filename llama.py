f9defefaaa57d7ee7edd24916e42ccdca61fdece
Li, Tingqian <tingqian.li@intel.com>
2023-11-06 22:23:16 +0800

    Support system message

--- llama.py
+++ llama.py
@@ -81,8 +81,81 @@ def make_rms_norm(key, input, consts, variance_epsilon, name_suffix=''):
 
 inv_freq = None
 
-def make_mha(query_states, key_states, value_states,
-             kv_cache, kv_cache_mask, beam_table, position_id,
+class KVCache:
+    def __init__(self, configs, batch_size, max_kv_len, verbose) -> None:
+        self.cache = torch.zeros(configs["layer_num"] * 2, batch_size, configs["head_num"], max_kv_len, configs["head_size"], dtype=torch.float32)
+        self.mask = torch.zeros(max_kv_len,  dtype=torch.int32)
+        self.max_kv_len = max_kv_len
+        self.position_id_next = 0
+        self.slots = []
+        self.cur_slot = 0
+        self.cur_kv_len = 0
+        self.beam_table = torch.zeros(batch_size, max_kv_len, dtype=torch.int32)
+        for b in range(batch_size):
+            self.beam_table[b,:] = b
+        
+        self.verbose = verbose
+
+    # the early stop check is vital to guarantee meaningful response
+    # because instruction (surrounded by [INST][/INST]) is required
+    # to produce meaningful response. 
+    def prepare(self, query_len, sentence_id, fail_on_sid):
+        # do we have cache entry can be overwriten ?
+        # mask[i]=  0 means no valid content
+        # mask[i]= -1 means system message
+        assert(query_len > 0)
+        if self.verbose:
+            print(f"qlen={query_len}, sid={sentence_id}, slot={self.cur_slot}")
+
+        cur_slot = self.cur_slot
+        self.slots = []
+        for k in range(query_len):
+            # skip cache slot reserved for system message
+            while self.mask[cur_slot] < 0:
+                cur_slot = (cur_slot + 1) % self.max_kv_len
+
+            # (sentence_id - 1) is sentence of instruct which
+            # must be part of valie context for meaningful response
+            if self.mask[cur_slot] != 0 and self.mask[cur_slot] == fail_on_sid:
+                if self.verbose:
+                    print(f" return false on self.mask[{cur_slot}] = {self.mask[cur_slot]}")
+                return False
+
+            self.slots.append(cur_slot)
+            cur_slot = (cur_slot + 1) % self.max_kv_len
+        self.cur_slot = cur_slot
+
+        # update the mask
+        for slot in self.slots:
+            self.mask[slot] = sentence_id
+
+        # update position_id
+        self.position_id = self.position_id_next
+        self.position_id_next = self.position_id + query_len
+
+        # update total kv length
+        self.cur_kv_len += query_len
+        if self.cur_kv_len > self.max_kv_len:
+            self.cur_kv_len = self.max_kv_len
+
+        if self.verbose:
+            smask= "".join([str(int(self.mask[i])) if self.mask[i] >= 0 else "N" for i in range(self.mask.shape[0])])
+            sslots = ",".join(str(s) for s in self.slots)
+            print(f"\t{smask}")
+            print(f"\t{sslots}")
+
+        return True
+
+    def update_cache(self, layer_idx, key_states, value_states):
+        q_len = key_states.shape[2]
+        for k in range(q_len):
+            self.cache[2*layer_idx + 0, :, :, self.slots[k], :] = key_states[:, :, k, :]
+            self.cache[2*layer_idx + 1, :, :, self.slots[k], :] = value_states[:, :, k, :]
+
+        # us beam_idx to gather(reorder kv cache), skipped in greedy case
+        return self.cache[2*layer_idx + 0, :, :, :self.cur_kv_len, :], self.cache[2*layer_idx + 1, :, :, :self.cur_kv_len, :]
+
+def make_mha(query_states, key_states, value_states, kv_cache,
              layer_idx, rotary_dim, hidden_size, num_heads, name):
     global inv_freq
     head_dim = hidden_size//num_heads
@@ -101,7 +174,7 @@ def make_mha(query_states, key_states, value_states,
     def rope_embedd(x):
         half_rotary_dim = rotary_dim//2
         for k in range(q_len):
-            cur_position_id = position_id + k
+            cur_position_id = kv_cache.position_id + k
             for i0 in range(half_rotary_dim):
                 i1 = i0 + half_rotary_dim
                 xita = (inv_freq[i0] * cur_position_id)
@@ -115,28 +188,17 @@ def make_mha(query_states, key_states, value_states,
     rope_embedd(query_states)
     rope_embedd(key_states)
 
-    max_kv_len = kv_cache.shape[3]
-
     # kv_cache is a circular buffer, and tokens should be overwritten in word boundary
 
-    # kv_cache [2 * n_layers, batch, n_head, max_kv_len, head_size]
-    for k in range(q_len):
-        pos_k = (position_id + k) % max_kv_len
-        kv_cache[2*layer_idx + 0, :, :, pos_k, :] = key_states[:, :, k, :]
-        kv_cache[2*layer_idx + 1, :, :, pos_k, :] = value_states[:, :, k, :]
+    key_states, value_states = kv_cache.update_cache(layer_idx, key_states, value_states)
 
-    kv_seq_len = position_id + q_len
-    if kv_seq_len > max_kv_len:
-        kv_seq_len = max_kv_len
-    # us beam_idx to gather(reorder kv cache), skipped in greedy case
-    key_states = kv_cache[2*layer_idx + 0, :, :, :kv_seq_len, :]
-    value_states = kv_cache[2*layer_idx + 1, :, :, :kv_seq_len, :]
-    kv_mask = kv_cache_mask[:kv_seq_len]
+    kv_seq_len = kv_cache.cur_kv_len
+    kv_mask = kv_cache.mask[:kv_seq_len]
 
     attn_weights = torch.matmul(query_states, key_states.transpose(2, 3)) / math.sqrt(head_dim)
 
     # mask out attn weight for kv-tokens whose [kv_cache_mask == 0]
-    attn_weights[:, :, :, kv_mask==0] = torch.finfo(torch.float32).min
+    # attn_weights[:, :, :, kv_mask==0] = torch.finfo(torch.float32).min
 
     # apply causal mask, so:
     #    q-token[q_len-1] can use all kv-tokens
@@ -148,7 +210,7 @@ def make_mha(query_states, key_states, value_states,
     for k in range(q_len-1):
         tokens_to_remove = (q_len - 1 - k)
         for d in range(tokens_to_remove):
-            pos = (position_id + q_len - 1 - d) % max_kv_len
+            pos = kv_cache.slots[q_len - 1 - d]
             attn_weights[:, :, k, pos] = torch.finfo(torch.float32).min
 
     attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
@@ -158,7 +220,7 @@ def make_mha(query_states, key_states, value_states,
     return attn_output
 
 
-def make_layer(configs, consts, layer_idx, hidden_states, position_id, kv_cache, kv_cache_mask, beam_table):
+def make_layer(configs, consts, layer_idx, hidden_states, kv_cache):
     name_suffix = f'.layer{layer_idx}'
     name_prefix = 'model.layers.self_attn'
     # layerNorm operation
@@ -168,7 +230,7 @@ def make_layer(configs, consts, layer_idx, hidden_states, position_id, kv_cache,
     k = make_fc('model.layers.self_attn.k_proj', input_layernorm, consts['layers'][layer_idx], name_suffix)
     v = make_fc('model.layers.self_attn.v_proj', input_layernorm, consts['layers'][layer_idx], name_suffix)
 
-    attn_output = make_mha(q, k, v, kv_cache, kv_cache_mask, beam_table, position_id,
+    attn_output = make_mha(q, k, v, kv_cache,
                            layer_idx, configs['rotary_dims'], configs['hidden_size'], configs['head_num'],
                            name=f'{name_prefix}.mha{name_suffix}')
 
@@ -195,11 +257,11 @@ def make_layer(configs, consts, layer_idx, hidden_states, position_id, kv_cache,
 # kv_cache   : [2 * n_layers, batch, n_head, max_kv_len, head_size]
 # beam_table : [batch, max_kv_len]
 # attn_mask  : [batch, query_len+past_len]
-def model_forward(configs, consts, input_ids, position_id, kv_cache, kv_cache_mask, beam_table):
+def model_forward(configs, consts, input_ids, kv_cache):
     inputs_embeds = F.embedding(input_ids, consts['model.embed_tokens.weight'])
     hidden_states = inputs_embeds
     for i in range(configs['layer_num']):
-        hidden_states = make_layer(configs, consts, i, hidden_states, position_id, kv_cache, kv_cache_mask, beam_table)
+        hidden_states = make_layer(configs, consts, i, hidden_states, kv_cache)
 
     final_layernorm = make_rms_norm('model.norm', hidden_states, consts, configs['rms_norm_eps'])
     logits = make_fc('lm_head', final_layernorm, consts)
@@ -207,7 +269,7 @@ def model_forward(configs, consts, input_ids, position_id, kv_cache, kv_cache_ma
 
 #=================================================================
 # simple greedy pipeline using model_forward
-def simple_pipeline(hf_model_path):
+def simple_chat_pipeline(hf_model_path, max_kv_len, system_message, verbose):
     global inv_freq
     print(f"load Tokenizer from {hf_model_path}...")
     tokenizer = AutoTokenizer.from_pretrained(hf_model_path, trust_remote_code=True)
@@ -224,98 +286,69 @@ def simple_pipeline(hf_model_path):
     rope_base = 10000
     inv_freq = 1.0 / (rope_base ** (torch.arange(0, configs["rotary_dims"], 2).float().to("cpu") / configs["rotary_dims"]))
 
-    batch_size = 1
-    max_kv_len = 64
-    kv_cache = torch.zeros(configs["layer_num"] * 2, batch_size, configs["head_num"], max_kv_len, configs["head_size"], dtype=torch.float32)
-    position_id = 0
-    beam_table = np.zeros([batch_size, max_kv_len], dtype=np.int32)
-    for b in range(batch_size):
-        beam_table[b,:] = b
-
-    next_tokens = None
+    if max_kv_len > configs["max_position_embeddings"]:
+        max_kv_len = configs["max_position_embeddings"]
     print(f"max_kv_len = {max_kv_len}")
 
-    segid = 0
-    kv_cache_mask = torch.zeros(max_kv_len,  dtype=torch.int32)
+    batch_size = 1
+    kv_cache = KVCache(configs, batch_size, max_kv_len, verbose)
 
-    # circular kv-cache means some tokens will be overwriten
-    # by new kv-cache
-    def set_kv_cache_mask(p0, len, mask_id):
-        assert(len < max_kv_len)
+    next_tokens = None
 
-        # Following logic clears whole segment to make rooms for new kv-tokens
-        # to prevent partial tokens to be in the kv-cache context, but it seems
-        # to be not neccessary. so we comment it out.
-
-        #id_to_remove = kv_cache_mask[(p0 + len - 1) % max_kv_len]
-        #if id_to_remove > 0 and id_to_remove != mask_id:
-        #    kv_cache_mask[kv_cache_mask == id_to_remove] = 0
-
-        i0 = p0 % max_kv_len
-        i1 = (p0 + len) % max_kv_len
-        if i1 > i0:
-            kv_cache_mask[i0:i1] = mask_id
-        else:
-            kv_cache_mask[i0:] = mask_id
-            kv_cache_mask[:i1] = mask_id
-
-        #smask= "".join([str(int(kv_cache_mask[i])) for i in range(max_kv_len)])
-        #print("\t\t", p0, len, mask_id, id_to_remove)
-        #print(smask)
+    sentence_id = 0
 
     with torch.no_grad():
+        sys_msg = system_message
         while True:
-            # attn_mask : [batch, query_len+past_len]
+            if sys_msg:
+                # sentence id <0 is reserved, need to skip when update kv-cache 
+                inputs = tokenizer(f"[INST] <<SYS>> {sys_msg} <</SYS>> [/INST]", return_tensors="pt", padding=True, return_token_type_ids=False)
+                input_ids = inputs["input_ids"]
+                assert(kv_cache.prepare(input_ids.shape[1], -1, -1))
+                logits = model_forward(configs, consts, input_ids, kv_cache)
+                sys_msg = None
+
             print("\033[0;32m")
             try:
                 prompt = input(">")
             except EOFError:
                 break
+
+            # kv-segment for question/instruction part
+            sentence_id += 1
+
             inputs = tokenizer(f"[INST] {prompt} [/INST]", return_tensors="pt", padding=True, return_token_type_ids=False)
-            #inputs = tokenizer(f"{prompt}", return_tensors="pt", padding=True, return_token_type_ids=False)
             input_ids = inputs["input_ids"]
 
             # append last predicted token(usually it's EOS)
             if next_tokens is not None:
                 input_ids = torch.cat((next_tokens, input_ids), dim=1)
-
-            # kv-segment for question/instruction part
-            segid += 1
+            assert(kv_cache.prepare(input_ids.shape[1], sentence_id, sentence_id))
 
             # logits    : [batch, q_len, vocab_size]
             first_tok_latency = time.time()
-            set_kv_cache_mask(position_id, input_ids.shape[1], segid)
-            logits = model_forward(configs, consts, input_ids, position_id, kv_cache, kv_cache_mask, beam_table)
+            logits = model_forward(configs, consts, input_ids, kv_cache)
             first_tok_latency = time.time() - first_tok_latency
 
-            position_id += logits.shape[1]
-
-            # only the last token
+            # only the last token in instruct predict the next
             next_token_logits = logits[:, -1, :]
             next_tokens = torch.argmax(next_token_logits, dim=-1).reshape(batch_size, 1)
 
-            # segid for response
-            segid += 1
+            # sentence_id for response
+            sentence_id += 1
             print("\033[0;33m")
             streamer.put(next_tokens)
             second_tok_count = 0
             second_tok_latency = time.time()
             early_stop = None
             while tokenizer.eos_token_id not in next_tokens:
-                input_ids = next_tokens
-
-                # the early stop check is vital to guarantee meaningful response
-                # because instruction (surrounded by [INST][/INST]) is required
-                # to produce meaningful response. 
-                id_to_remove = int(kv_cache_mask[position_id % max_kv_len])
-                if id_to_remove == (segid - 1):
+                if not kv_cache.prepare(1, sentence_id, sentence_id - 1):
                     early_stop = "...(Early stop before instruction gets overwritten)"
                     break
 
-                set_kv_cache_mask(position_id, 1, segid)
-                logits = model_forward(configs, consts, input_ids, position_id, kv_cache, kv_cache_mask, beam_table)
+                input_ids = next_tokens
+                logits = model_forward(configs, consts, input_ids, kv_cache)
 
-                position_id += 1
                 second_tok_count += 1
                 next_tokens = torch.argmax(logits, dim=-1).reshape(batch_size, 1)
                 streamer.put(next_tokens)
@@ -324,13 +357,16 @@ def simple_pipeline(hf_model_path):
             if early_stop:
                 print("\033[0;31m", early_stop)
 
-            print("\033[0;90m", f" position_id: {position_id}  latency: {first_tok_latency*1e3:.2f} ms + {second_tok_latency*1e3:.2f}ms x {second_tok_count}")
+            print("\033[0;90m", f" position_id: {kv_cache.position_id}  latency: {first_tok_latency*1e3:.2f} ms + {second_tok_latency*1e3:.2f}ms x {second_tok_count}")
             print("\033[00m")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser('')
     parser.add_argument('--org_model_path', type=str, nargs='?', default='/home/llm_irs/models_original/llama-2-7b-chat/pytorch/')
+    parser.add_argument('--sys', type=str, default=None)
+    parser.add_argument('--kv-len', type=int, default=128)
+    parser.add_argument('-v', '--verbose', action="store_true")
     parser.add_argument('--quant_type', type=str, nargs='?', default='')
     args = parser.parse_args()
-    simple_pipeline(args.org_model_path)
+    simple_chat_pipeline(args.org_model_path, args.kv_len, args.sys, args.verbose)
